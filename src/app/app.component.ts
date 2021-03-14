09dea6c67302de61a023b6632c563690c91c802a
DESKTOP-5GK56BC\flouk <flouk.cs07@gmail.com>
2021-03-14 18:44:30 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,65 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, merge, Observable } from 'rxjs';
+import { debounceTime, map, mapTo, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  num!: number;
   title = 'pretest';
+  type = 'isPrime';
+  result!: boolean; 
+  @ViewChild('input') input!: ElementRef<HTMLInputElement>;
+  @ViewChild('typeselect') typeElm!: ElementRef;
+
+  result$!: Observable<boolean>;
+
+
+
+  ngAfterViewInit() {
+    const inputElm$ =  fromEvent<Event>(this.input.nativeElement, 'input').pipe(map(event => {
+      const target = (event.target as HTMLInputElement);
+      let value = +target.value;
+  
+      if (!Number.isInteger(value)) {
+        this.num = Math.round(value);
+      }
+  
+      if (value < 0) {
+        this.num = 1;
+      }
+  
+      if (!value) {
+        this.num = 0;
+      }
+      return this.num;
+    }));
+    const typeElm$ =   fromEvent(this.typeElm.nativeElement, 'change').pipe(map(()=>this.num));
+    this.result$  = merge( inputElm$  ,typeElm$ ).pipe(map(val=>{
+      if(this.type === 'isPrime') {
+        return  this.isPrime(val);
+      } else {
+        return  this.isFibonacci(val);
+      }
+    }));
+  }
+
+  isPrime(num: number) {
+    for (let i = 2; i < num; i++)
+      if (num % i === 0) return false;
+    return num > 1;
+  }
+
+  isFibonacci = (num: number) => {
+    if (this.isSquare(5 * (num * num) - 4) || this.isSquare(5 * (num * num) + 4)) {
+      return true;
+    } else { return false; }
+  };
+
+  isSquare = (n: number) =>{
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+  }
+
 }
